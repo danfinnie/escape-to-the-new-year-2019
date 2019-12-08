affcc4d0351230712389b1ff3f7619330758569f
Daniel Finnie <dan@danfinnie.com>
2019-12-08 16:23:38 -0500

    Add postgres, route to print DB info

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 
 class App < Sinatra::Base
   get "/" do
     erb :index
+  end
+
+  get "/env" do
+    erb :env, locals: {
+	  host: ENV['DATABASE_HOST'],
+	  database: ENV['DATABASE_NAME'],
+	  username: ENV['DATABASE_USER'],
+	  password: ENV['DATABASE_PASSWORD']
+     }
   end
 end
